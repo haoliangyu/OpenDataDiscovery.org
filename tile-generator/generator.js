34b7782060d0ba126416c4dbba4689aea099a211
haoliangyu <njkon@foxmail.com>
2016-08-11 22:34:55 -0400

    allow min/max zoom level for each vector tile layer

--- tile-generator/generator.js
+++ tile-generator/generator.js
@@ -104,7 +104,7 @@ exports.preseed = function(instanceID, db) {
 
       _.forEach(layers, function(layer) {
         var url = params.tileBaseUrl + layer;
-        var filePath = sprintf('.tile-server/tiles/%s.mbtiles', layer);
+        var filePath = sprintf('./tile-server/tiles/%s.mbtiles', layer);
         serverConfig[url] = 'mbtiles://' + filePath;
       });
 
