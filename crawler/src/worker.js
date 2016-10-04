f2221d9a61827ca1c1343d41764d68aa665c1764
haoliangyu <njkon@foxmail.com>
2016-10-04 00:41:47 -0400

    fix broken crawler due to the db schema change

--- crawler/src/worker.js
+++ crawler/src/worker.js
@@ -6,61 +6,41 @@ var database = require('./database.js');
 var pgp = require('pg-promise')({ promiseLib: Promise });
 var params = require('./params.js');
 
-exports.spatialCrawl = function(instanceName, instanceID, instanceUrl, queue, db) {
+exports.spatialCrawl = function(db, instanceID, instanceName, instanceUrl) {
 
   db = db || pgp(params.dbConnStr);
 
   var sql = 'SELECT region_id, Box2D(bbox) AS bbox FROM view_instance_region WHERE instance_id = $1';
 
-  return db.any(sql, instanceID)
-  .then(function(regions) {
-
-    _.forEach(regions, function(region) {
-
+  return db.one(sql, instanceID)
+    .then(function(region) {
       var match = region.bbox.match(/BOX\(([-.0-9]+) ([-.0-9]+),([-.0-9]+) ([-.0-9]+)\)/);
       var bbox = _.chain(match).slice(1, 5).map(_.toNumber).value();
-      var promise = ckan.getFullMetadata(instanceUrl, {
+
+      return ckan.getFullMetadata(instanceUrl, {
         extras: { ext_bbox: bbox }
       })
       .then(function(data) {
         return db.tx(function(t) {
           return database.saveData(t, instanceID, region.region_id, data);
         });
-      })
-      .catch(function(err) {
-        logger.error(err);
       });
-
-      queue.add(function() { return promise; });
+    })
+    .catch(function(err) {
+      logger.error(err);
+      return Promise.resolve();
     });
-  })
-  .catch(function(err) {
-    switch (err) {
-      case 'no region':
-        logger.info('No region is found for instance: ', instanceID);
-        return Promise.resolve();
-      default:
-        logger.warn('Failed to fetch data from ' + instanceUrl);
-        logger.error(err);
-    }
-  });
 };
 
-exports.crawl = function(instanceName, instanceID, instanceUrl, queue, db) {
+exports.crawl = function(db, instanceID, instanceName, instanceUrl) {
 
   db = db || pgp(params.dbConnStr);
 
   logger.info('Crawling ' + instanceName + '...');
 
-  var sql = [
-    'SELECT region_id FROM instance_region_xref AS irx',
-    'LEFT JOIN instance_region_level as irl ON irl.id = irx.instance_region_level_id',
-    'WHERE irx.instance_id = $1 ORDER BY irl.level LIMIT 1'
-  ].join(' ');
-
   var regionID;
 
-  var promise = db.one(sql, instanceID)
+  return db.one('SELECT region_id FROM instance_region_xref WHERE instance_id = $1', instanceID)
     .then(function(result) {
       regionID = result.region_id;
       return ckan.getFullMetadata(instanceUrl);
@@ -74,6 +54,4 @@ exports.crawl = function(instanceName, instanceID, instanceUrl, queue, db) {
       logger.error(err);
       return Promise.resolve();
     });
-
-  queue.add(function() { return promise; });
 };
