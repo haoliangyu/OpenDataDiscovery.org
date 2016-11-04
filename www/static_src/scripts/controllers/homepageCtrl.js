52e1acfff6591a92ce9fb95364838e212450c591
haoliangyu <njkon@foxmail.com>
2016-11-04 16:39:27 -0400

    another trial to fix issue #36

--- www/static_src/scripts/controllers/homepageCtrl.js
+++ www/static_src/scripts/controllers/homepageCtrl.js
@@ -20,6 +20,23 @@ class homepageCtrl {
       .catch(err => {
         console.error(err);
       });
+
+    /**
+     * Modify the base url at base tag, source:
+     * http://stackoverflow.com/questions/34317038/adding-base-tag-href-with-javascript-doesnt-work
+     */
+
+    // Fetch base element and href attribute
+    let baseElement = document.getElementsByTagName('base')[0];
+    let currentBaseHrefValue = baseElement.getAttribute('href');
+
+    // Get correct domain value
+    let urlForBase = window.location.href;
+    let urlForBaseArr = urlForBase.split('/');
+    let baseHrefUrl = urlForBaseArr[0] + '//' + urlForBaseArr[2] + '/' + currentBaseHrefValue;
+
+    // Update base with domain to make it absolute for IE9 and IE10
+    baseElement.setAttribute("href", baseHrefUrl);
   }
 
   scrollTo(elementID) {
