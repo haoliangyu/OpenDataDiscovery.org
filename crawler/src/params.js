3a74bb4438e12f5348cb721c470de7362f01076d
haoliangyu <njkon@foxmail.com>
2016-11-06 15:50:31 -0500

    increase data limit

--- crawler/src/params.js
+++ crawler/src/params.js
@@ -1,10 +1,10 @@
 exports.dbConnStr = 'postgres://odd_admin:Bko9tu39@localhost:5432/odd';
 
 exports.searchLimit = {
-  tag: 1000,
-  organization: 1000,
-  group: 100,
-  default: 1000
+  tag: 100000,
+  organization: 100000,
+  group: 100000,
+  default: 100000
 };
 
 exports.minWait = 0;
